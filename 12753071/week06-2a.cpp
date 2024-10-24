0fd1fd399426ac01d00ed2da53e3154e4ac7f419
12753071 <jackhuang816@gmail.com>
2024-10-24 13:23:11 +0800

    Add files via upload

---
+++ 12753071/week06-2a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+///week06-2a.cpp
+#include <vector>
+using namespace std;
+
+int main(){
+	int now;
+	while(cin >> now){///step01 input
+
+		cout << now << "\n";///step02 output
+	}
+}
